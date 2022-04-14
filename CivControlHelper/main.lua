7b839574af0806c47804d8e4b8cae43494961457
katze0010 <84671638+katze0010@users.noreply.github.com>
2022-04-14 19:55:55 +0000

    Add files via upload

---
+++ CivControlHelper/main.lua
@@ -0,0 +1,30 @@
+local _upd_criminal_suspicion_progress_original = GroupAIStateBase._upd_criminal_suspicion_progress
+function GroupAIStateBase:_upd_criminal_suspicion_progress(...)
+	if self._ai_enabled then
+		for obs_key, obs_susp_data in pairs(self._suspicion_hud_data or {}) do
+			local unit = obs_susp_data.u_observer
+			
+			if managers.enemy:is_civilian(unit) then
+				local waypoint = managers.hud._hud.waypoints["susp1" .. tostring(obs_key)]
+				
+				if waypoint then
+					local color, arrow_color
+					
+					if unit:anim_data().drop then
+						if not obs_susp_data._subdued_civ then
+							obs_susp_data._alerted_civ = nil
+							obs_susp_data._subdued_civ = true
+							color = Color(0, 1, 0)
+							arrow_color = Color(0, 1, 0)
+						end
+					end
+					if color then
+						waypoint.bitmap:set_color(color)
+						waypoint.arrow:set_color(arrow_color:with_alpha(0.75))
+					end
+				end
+			end
+		end
+	end
+	return _upd_criminal_suspicion_progress_original(self, ...)
+end
